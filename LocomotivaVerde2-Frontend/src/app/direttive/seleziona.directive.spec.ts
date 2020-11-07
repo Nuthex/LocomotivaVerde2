48e5e949ba6183142fa7d445a88201805140323e
Nuthex <72572263+Nuthex@users.noreply.github.com>
2020-11-07 22:22:36 +0100

    22:22 07/11/2020 Push

--- LocomotivaVerde2-Frontend/src/app/direttive/seleziona.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SelezionaDirective } from './seleziona.directive';
-
-describe('SelezionaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SelezionaDirective();
-    expect(directive).toBeTruthy();
-  });
-});
